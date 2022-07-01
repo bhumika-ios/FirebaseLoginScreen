4dec912938ab9a5e54f89d9c69d9edd5a3ac84e4
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-07-01 12:19:08 +0530

    Initial Commit

---
+++ FirebaseLoginScreen/FirebaseLoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLoginScreenApp.swift
+//  FirebaseLoginScreen
+//
+//  Created by Bhumika Patel on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
